88ea1b50d62bc8dd14fa48337081ff6a9d68aae8
Paulo Henrick <pauloifgoianorv@gmail.com>
2022-03-28 21:45:38 -0300

    Arquivos iniciais

---
+++ paralelo1.c
@@ -0,0 +1,159 @@
+#include <omp.h>
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//cabeçalho das funçoes
+void alocar();
+void limparMemoria();
+void resolverSistema();
+void mostrarResultado();
+void escreverMatrizA(double **a, int n);
+void escreverVetorB(double *b, int n);
+void preencherMatrizA(double **a, int n);
+void preencherVetorB(double *b, int n);
+
+int n, t, i;
+double **a, *b, *resultado, tempoInicial, tempoFinal, tempoGasto;
+
+int main(int argc, char*argv[]){
+
+    printf("\nSolução do sistema linear (Ax=b) com o Método de Gauss.\n\n");
+
+    if(argc > 1){
+		n = atoi(argv[1]);
+        t = atoi(argv[2]);
+        omp_set_num_threads(t);
+		printf("Matriz de ordem: %d\n\n", n);
+    }
+
+    alocar(n);
+    if(a != NULL){
+        resolverSistema();
+        limparMemoria();
+    }else{
+        printf("Erro");
+    }
+    return 0;
+}
+
+void alocar(int n){ 
+    // alocando espaço na memória para a Matriz A
+    a = malloc(sizeof(double *) * n);
+    for (int i=0; i<n; i++) {
+        a[i] = malloc(sizeof(double) * n);
+    }
+
+    // alocando espaço na memória para o Vetor b
+    b = malloc(sizeof(double) * n);
+
+    // alocando espaço de memória para o vetor resultado
+    resultado = malloc(sizeof(double) * n);
+    
+    preencherMatrizA(a, n);
+    preencherVetorB(b, n);
+    escreverMatrizA(a, n);
+    escreverVetorB(b, n);
+}
+
+// limpando a memória alocada
+void limparMemoria(){
+    for (int i=0; i<n; i++) {
+        free(a[i]);
+    }
+    free(a);
+    free(b);
+    free(resultado);
+}
+
+void resolverSistema(){
+    tempoInicial = omp_get_wtime();
+    #pragma omp parallel default(none) shared(a, b, n, resultado)
+    {
+    int ID = omp_get_thread_num();
+    printf("ID da Thread (%d)\n", ID);
+
+    // eliminação
+    #pragma omp for
+    for (int i=0; i<n-1; i++) {
+        for (int j=i+1; j<n; j++) {
+            double x;
+            x = -a[j][i] / a[i][i];
+            for (int k=0; k<n; k++) {
+                a[j][k] += a[i][k] * x;
+            }
+            b[j] += b[i] * x;
+        }
+    }
+
+    // substituição regressiva
+    #pragma omp for
+    for (int i=n-1; i>=0; i--) {
+        double x = 0;
+        for (int j=i+1; j<n; j++) {
+            x += a[i][j] * resultado[j];
+        }
+        x = -x + b[i];
+        resultado[i] = x / a[i][i];
+    }
+    }
+    mostrarResultado();
+
+    // capturar o tempo
+    tempoFinal = omp_get_wtime();
+    tempoGasto = tempoFinal - tempoInicial;
+    printf("\nEsse computador calculou todos os valores em %g (s)\n", tempoGasto);
+}
+
+void mostrarResultado(){
+    printf("\nO reultado é: \n");
+    printf("x = ");
+    
+    float tst = resultado[0];
+    int x1 = isnan(tst);
+    if(x1 != 0) {
+        printf("Erro");
+        printf("\n\n");
+        return;
+    }
+    
+    for (int i=0; i<n; i++) {
+        printf("%0.3lf ", resultado[i]);
+    }
+    printf("\n\n");
+}
+
+void escreverMatrizA(double **a, int n){
+    printf("Matriz A: \n");
+    for(int i=0; i < n; i++){     
+       		for(int j=0; j < n; j++){    
+        		printf("%f\t", a[i][j]);
+			}
+			printf("\n\n");
+		} 
+}
+
+void escreverVetorB(double *b, int n){
+    printf("Vetor b: \n");
+    for(int i=0; i < n; i++){     
+        printf("%f\t", b[i]);
+	}
+    printf("\n"); 
+}
+
+void preencherMatrizA(double **a, int n){
+    srand(time(NULL));
+    for(int i=0; i < n; i++){     
+       	for(int j=0; j < n; j++){    
+            a[i][j] = rand() % 50;
+		}
+	}
+}
+
+void preencherVetorB(double *b, int n){
+    srand(time(NULL));
+    for(int i=0; i < n; i++){        
+        b[i] = rand() % 30;
+	}
+}
